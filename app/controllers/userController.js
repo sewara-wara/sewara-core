40f57502b65bc16d16638f6aa427331c5768e7a1
Samsud Dhuha <suddhuha@gmail.com>
2025-02-26 06:29:30 +0700

    update handle error

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -5,7 +5,7 @@ const errorHandler = require("../middleware/error.js");
 
 exports.getUsers = async (req, res) => {
     try {
-        const [users] = await db.query('SELECT * FROM user');
+        const [users] = await db.pool.query('SELECT * FROM user');
 
         res.status(statusCode.success).json({
             code: statusCode.success,
--- app/middleware/error.js
+++ app/middleware/error.js
@@ -1,24 +1,22 @@
 const statusCode = require('../config/statusCode.js');
 
-exports.error = (error, response) => {
+exports.error = (error, code, message, response) => {
     if (error) {
+        console.error(error);
         return response.status(statusCode.bad_request).json({
             code: statusCode.bad_request,
             message: error.message,
             error: error
         });
+    } else {
+        const error = new Error(message);
+        error.statusCode = code || 500;
+        
+        console.error(error);
+        return response.status(code).json({
+            code: code,
+            message: message || 'Internal Server Error',
+            error: error
+        });
     }
-}
-
-exports.error = (statusCode, message, response) => {
-    const code = statusCode || 500;
-    const error = new Error(message);
-    error.statusCode = code;
-    console.error(error);
-    
-    return response.status(code).json({
-        code: code,
-        message: message || 'Internal Server Error',
-        error: error
-    });
 }
