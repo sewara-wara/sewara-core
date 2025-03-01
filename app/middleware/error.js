d127e083ec7f8be1ce36ad9624e9ec2ed166064a
Samsud Dhuha <suddhuha@gmail.com>
2025-03-01 08:34:27 +0700

    revert handle error

--- app/middleware/error.js
+++ app/middleware/error.js
@@ -3,7 +3,7 @@ const statusCode = require('../config/statusCode.js');
 exports.error = (error, response) => {
     if (error) {
         console.error(error);
-        return response.json({
+        return response.status(statusCode.bad_request).send({
             code: statusCode.bad_request,
             message: error.message,
             error: error
