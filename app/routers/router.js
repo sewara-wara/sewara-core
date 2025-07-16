2e04c649c73df731e51a54d53e5669be148e228f
Samsud Dhuha <suddhuha@gmail.com>
2025-07-16 08:46:29 +0700

    test bcrypt

--- app/routers/router.js
+++ app/routers/router.js
@@ -20,14 +20,6 @@ router.get('/api/user', user.getUser);
 router.get('/api/user/detail', auth.verifyToken, user.getUserDetail);
 router.get('/api/user/update', auth.verifyToken, user.updateUser);
 
-router.post('/test-bcrypt', async (req, res) => {
-    const { raw, hash } = req.body;
-    const isMatch = await bcrypt.compare(raw, hash);
-    const hashed = await bcrypt.hash(raw, 10);
-    res.json({ hashed, match: isMatch });
-});
-
-
 // const uploadController = require("../controllers/uploadController.js");
 // router.post("/api/upload", auth.verifyToken, upload.single("file"), uploadController.upload);
 
